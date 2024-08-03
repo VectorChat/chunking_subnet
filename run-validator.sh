06a1e35c0586716adae54fdbc6d87a74d3abc4d8
VectorChat <dev@vectorchat.ai>
2024-08-03 18:32:21 +0000

    new run-validator script

--- run-validator.sh
+++ run-validator.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 source .env
 
-bash validator-autoupdate.sh --netuid $NETUID --wallet.name $COLDKEY --wallet.hotkey $HOTKEY --log_level debug --openaikey $OPENAI_API_KEY
+NAME=chunking_validator_autoupdate
+
+if pm2 describe $NAME > /dev/null 2>&1; then
+    echo "Process '$NAME' is running. Stopping it..."
+    pm2 stop $NAME
+    pm2 delete $NAME
+fi
+
+pm2 start validator-autoupdate.sh --name $NAME -- --netuid $NETUID --wallet.name $COLDKEY --wallet.hotkey $HOTKEY --log_level debug --openaikey $OPENAI_API_KEY
