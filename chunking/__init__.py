31c6f396d60426e525966d209dcfff9434e8c5b0
VectorChat <dev@vectorchat.ai>
2024-08-06 00:30:35 -0500

    1.0.6

--- chunking/__init__.py
+++ chunking/__init__.py
@@ -16,7 +16,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 version_split = __version__.split(".")
 __spec_version__ = (
     (1000 * int(version_split[0]))
