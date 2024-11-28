cc5faf808492f26d89155d28292040d20fa56d5a
VectorChat <dev@vectorchat.ai>
2024-11-28 13:01:21 -0600

    naive check for chunk ending on sentence boundary; accompanying tests

--- tests/test_reward_fn.py
+++ tests/test_reward_fn.py
@@ -3,26 +3,28 @@ import logging
 from math import ceil, e
 from random import sample
 from openai import AsyncOpenAI, OpenAI
+import pytest
 from chunking.protocol import chunkSynapse
+from chunking.utils.chunks import calculate_chunk_qty
 from chunking.utils.synthetic import generate_doc_normal
 from chunking.validator.reward import reward
 
-from tests.utils import base_chunker
+from tests.utils.articles import get_articles
+from tests.utils.chunker import base_chunker, mid_sentence_chunker
+from tests.utils.synthetic import get_or_load_synthetic_data
 
 logger = logging.getLogger(__name__)
 
 
-async def run_test():
-    PAGE_ID = 33653136
-
-    tuple = await generate_doc_normal(None, PAGE_ID)
-
-    document = tuple[0]
+async def run_test(document: str):
 
     timeout = 20
     time_soft_max = timeout * 0.75
     chunk_size = 4096
-    chunk_qty = ceil(ceil(len(document) / chunk_size) * 1.5)
+    chunk_qty = calculate_chunk_qty(
+        document=document,
+        chunk_size=chunk_size,
+    )
     synapse = chunkSynapse(
         document=document,
         time_soft_max=time_soft_max,
@@ -56,6 +58,18 @@ async def run_test():
 
     # reward should be 0 if no chunks
     logger.info("reward should be 0 if no chunks")
+
+    reward_value, _ = await _calculate_reward(
+        synapse, do_checks=True, do_penalties=False
+    )
+
+    assert reward_value == 0
+
+    logger.info("reward should be zero if chunk does not split on sentence boundary")
+
+    synapse.chunks = mid_sentence_chunker(synapse.document, synapse.chunk_size)
+
+    logger.info(f"got {len(synapse.chunks)} chunks")
 
     reward_value, _ = await _calculate_reward(
         synapse, do_checks=True, do_penalties=False
@@ -172,19 +186,34 @@ async def run_test():
 
     test_chunks = base_chunker(synapse.document, synapse.chunk_size)
 
+    logger.info(f"got {len(test_chunks)} chunks")
+
     synapse.chunks = test_chunks
 
-    reward_value, _ = await _calculate_reward(
+    reward_value, extra_info = await _calculate_reward(
         synapse, do_checks=True, do_penalties=False
     )
 
+    embedding_reward = extra_info["embedding_reward"]
+    logger.info(f"embedding_reward: {embedding_reward}")
+
+    qty_penalty = extra_info["qty_penalty"]
+    logger.info(f"qty_penalty: {qty_penalty}")
+
+    size_penalty = extra_info["size_penalty"]
+    logger.info(f"size_penalty: {size_penalty}")
+
     assert reward_value > 0
+    assert size_penalty == 0
+    assert qty_penalty == 0
 
     # should penalize big_chunks
 
     logger.info("should penalize big_chunks")
 
     big_chunks = base_chunker(synapse.document, synapse.chunk_size * 2)
+
+    logger.info(f"got {len(big_chunks)} chunks")
 
     synapse.chunks = big_chunks
 
@@ -193,23 +222,35 @@ async def run_test():
     )
 
     embedding_reward = extra_info["embedding_reward"]
+    logger.info(f"embedding_reward: {embedding_reward}")
+
+    size_penalty = extra_info["size_penalty"]
+    logger.info(f"size_penalty: {size_penalty}")
+
     exp_embedding_reward = e**embedding_reward
 
     assert reward_value < exp_embedding_reward
+    assert size_penalty > 0
 
     logger.info("reward should not penalize big chunks if not doing penalties")
 
-    reward_value, _ = await _calculate_reward(
+    reward_value, extra_info = await _calculate_reward(
         synapse, do_checks=True, do_penalties=False
     )
 
-    assert reward_value > 1
+    size_penalty = extra_info["size_penalty"]
+    logger.info(f"size_penalty: {size_penalty}")
+
+    assert size_penalty == 0
+    assert reward_value > 0
 
     # should penalize lots of chunks
 
     logger.info("should penalize lots of chunks")
 
-    small_chunks = base_chunker(synapse.document, synapse.chunk_size // 2)
+    small_chunks = base_chunker(synapse.document, synapse.chunk_size // 4)
+
+    logger.info(f"got {len(small_chunks)} chunks")
 
     synapse.chunks = small_chunks
 
@@ -218,18 +259,59 @@ async def run_test():
     )
 
     embedding_reward = extra_info["embedding_reward"]
-    exp_embedding_reward = e**embedding_reward
+    qty_penalty = extra_info["qty_penalty"]
 
-    assert reward_value < exp_embedding_reward
+    logger.info(f"embedding_reward: {embedding_reward}")
+    logger.info(f"qty_penalty: {qty_penalty}")
+
+    assert reward_value > 0
+    assert reward_value < 1.1
+    assert qty_penalty > 0
 
     logger.info("reward should not penalize lots of chunks if not doing penalties")
 
-    reward_value, _ = await _calculate_reward(
+    logger.info(f"got {len(small_chunks)} chunks")
+
+    reward_value, extra_info = await _calculate_reward(
         synapse, do_checks=True, do_penalties=False
     )
 
-    assert reward_value > 1
+    qty_penalty = extra_info["qty_penalty"]
+
+    assert qty_penalty == 0
+    assert reward_value > 0
 
 
-def test_reward_fn():
-    asyncio.run(run_test())
+async def main(num_articles: int):
+    # PAGE_ID = 33653136
+    # tuple = await generate_doc_normal(None, PAGE_ID)
+    # document = tuple[0]
+
+    all_pageids = get_articles()
+
+    logger.info(f"Testing reward function with {num_articles} articles")
+
+    documents = await get_or_load_synthetic_data(
+        n=num_articles,
+        all_pageids=all_pageids,
+        k=3,
+        loop_range=range(3, 4),
+        aclient=AsyncOpenAI(),
+        synth_gen_batch_size=5,
+    )
+
+    logger.info(f"Got {len(documents)} documents")
+
+    for document, save_path in documents:
+        logger.info(
+            f"Testing document with {len(document)} characters from {save_path}"
+        )
+        await run_test(document)
+        logger.info(
+            f"Finished testing document with {len(document)} characters from {save_path}"
+        )
+
+
+# @pytest.mark.parametrize("num_articles", [5])
+def test_reward_fn(num_articles: int):
+    asyncio.run(main(num_articles))
